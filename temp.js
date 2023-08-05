91b45c4f3563178b10174753e2ceef09a0640465
my-first-github-repos1 <mdsajjadujjaman09@gmail.com>
2023-08-05 11:09:55 -0700

    second

--- temp.js
+++ temp.js
@@ -1,9 +1,17 @@
 let first = 5;
 let second = 7;
 console.log(first, second);
+
+// this is wrong approch
 // first = second;
 // second = first;
-const temp = first;
-first = second;
-second = temp;
-console.log(first, second);
+
+// approach 1
+// const temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// approach 2 ---- destructuring
+[first , second] = [second, first];
+console.log(first, second)
